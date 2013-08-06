7e8fad3d502059df82a5789138e1b06602bf2dec
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-08-06 16:24:43 +0200

    Generate more orgs.

--- test/example/grails-app/conf/BootStrap.groovy
+++ test/example/grails-app/conf/BootStrap.groovy
@@ -8,14 +8,19 @@ class BootStrap {
     def init = { servletContext ->
         def Random generator = new Random()
 
-        def firstOrg = new Org(id: 1, name: "First Org", num: "num 1")
+        def firstOrg = new Org(id: 1, name: "github", num: "111-111-111")
         firstOrg.save(flush: true)
 
-        def secondOrg = new Org(id: 2, name: "Second Org", num: "num 2")
+        def secondOrg = new Org(id: 2, name: "9ci", num: "222-222-222")
         secondOrg.save(flush: true)
 
-        def thirdOrg = new Org(id: 3, name: "Third Org", num: "num 3")
+        def thirdOrg = new Org(id: 3, name: "Microsoft", num: "333-333-333")
         thirdOrg.save(flush: true)
+
+        for (i in 0..50) {
+            def org = new Org(name: fakerService.companyName(), num: fakerService.numerify("##-##-##"))
+            org.save(flush: true)
+        }
 
         def orgs = [firstOrg, secondOrg, thirdOrg]
 
