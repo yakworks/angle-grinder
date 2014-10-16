3640cd05e053e2afa6b615830fbc0614bb382d4b
Sudhir Nimavat <sudhir_nimavat@yahoo.com>
2014-10-16 20:49:58 +0530

    9ci/rcm#795 - Move anglegrinder to assetpipeline

---
+++ grails/ag-plugin/grails-app/assets/javascripts/angleGrinder/vendor.js
@@ -0,0 +1,6 @@
+//This is a JavaScript file with its top level require directives
+//= require scripts/jquery.min.js
+//= require scripts/lodash.min.js
+//= require scripts/angularjs-all.min.js
+//= require scripts/bootstrap.min.js
+//= require scripts/bootstrap-extras.min.js
