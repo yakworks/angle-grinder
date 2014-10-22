7258817e44959cedf8da3e19fd5dafda92fc2cb3
Ken Roberts <ken@9ci.com>
2014-10-22 11:11:36 -0500

    Working _Events.groovy and reverted update-ag-plugin script

--- grails/ag-plugin/scripts/_Events.groovy
+++ grails/ag-plugin/scripts/_Events.groovy
@@ -10,18 +10,5 @@ eventCompileStart = {
 	println "ANGLE_GRINDER_DIR=${angleGrinderDir.absolutePath}"
 	File scripts = new File(angleGrinderDir,'script')
 	File updateAgPluginScript = new File(scripts, 'update-ag-plugin')
-	// println "update-ag-plugin script is at ${updateAgPluginScript.absolutePath}"
-	// println "The script ${updateAgPluginScript.exists()?'exists.':'does not exist.'}"
-	def scriptProc = [
-		updateAgPluginScript.absolutePath,
-		angleGrinderDir.absolutePath
-	].execute()
-	println "	...waiting"
-	scriptProc.waitFor()
-	println scriptProc.err.txt
-	println scriptProc.in.txt // this is 'out' for the script
-	if(scriptProc.exitValue()) {
-		def msg = "Failed to update-ag-plugin.  Exited with ${scriptProc.exitValue()}"
-		return
-	}
+	ant.exec(executable: updateAgPluginScript.absolutePath, dir: angleGrinderDir.absolutePath)
 }
