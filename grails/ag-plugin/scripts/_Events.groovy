db6d1bce87a0148a7a1466fe021a0f140ae91178
Ken Roberts <ken@9ci.com>
2014-10-27 13:09:16 -0500

    Removed the auto build of angle-grinder because it triggers in every project and fails there.

--- grails/ag-plugin/scripts/_Events.groovy
+++ grails/ag-plugin/scripts/_Events.groovy
@@ -5,13 +5,13 @@ eventCleanStart = {
 	Ant.delete('dir':'target')
 }
 
-eventCompileStart = {
-	File angleGrinderDir = new File(System.getProperty('user.dir')).parentFile.parentFile
-	println "ANGLE_GRINDER_DIR=${angleGrinderDir.absolutePath}"
-	File scripts = new File(angleGrinderDir,'script')
-	File updateAgPluginScript = new File(scripts, 'update-ag-plugin')
-	println "Executing script ${updateAgPluginScript.absolutePath}"
-	// FIXME get this script's output without using --verbose
-	ant.exec(executable: updateAgPluginScript.absolutePath, dir: angleGrinderDir.absolutePath)
-	println "...done."
-}
+//eventCompileStart = {
+//	File angleGrinderDir = new File(System.getProperty('user.dir')).parentFile.parentFile
+//	println "ANGLE_GRINDER_DIR=${angleGrinderDir.absolutePath}"
+//	File scripts = new File(angleGrinderDir,'script')
+//	File updateAgPluginScript = new File(scripts, 'update-ag-plugin')
+//	println "Executing script ${updateAgPluginScript.absolutePath}"
+//	// FIXME get this script's output without using --verbose
+//	ant.exec(executable: updateAgPluginScript.absolutePath, dir: angleGrinderDir.absolutePath)
+//	println "...done."
+//}
