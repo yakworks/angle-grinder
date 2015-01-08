225a7d76cf243babbd67ec9134952573ab9074e6
Sudhir <sudhir@nimavat.me>
2015-01-08 11:07:15 +0530

    Fix dependencies issue, remove dependency on fields plugin, upgrade hibernate plugin version, add grailsHome and maven centrl repos, comment out 9ci repo

--- grails/ag-plugin/grails-app/conf/BuildConfig.groovy
+++ grails/ag-plugin/grails-app/conf/BuildConfig.groovy
@@ -4,40 +4,46 @@ grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+  legacyResolve true
+  // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+  repositories {
+    grailsPlugins()
+    grailsHome()
+    mavenLocal()
+    grailsCentral()
+    mavenCentral()
+
+    //mavenRepo(id: '9ci-artifactory', url: 'http://repo.9ci.com/artifactory/repo')
+
+    // uncomment the below to enable remote dependency resolution
+    // from public Maven repositories
+    //mavenCentral()
+    //mavenRepo "http://snapshots.repository.codehaus.org"
+    //mavenRepo "http://repository.codehaus.org"
+    //mavenRepo "http://download.java.net/maven/2/"
+    //mavenRepo "http://repository.jboss.com/maven2/"
+  }
+
+  dependencies { }
+
+  plugins {
+    runtime(':hibernate:3.6.10.18') {
+      export = false
     }
 
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-
-    legacyResolve true // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
-
-    repositories {
-        grailsCentral()
-        mavenRepo name: '9ci', root: 'http://repo.9ci.com/artifactory/repo'
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+    build(":release:2.2.1", ":rest-client-builder:1.0.3") {
+      export = false
     }
 
-    dependencies {
-    }
-
-    plugins {
-      build ':tomcat:7.0.42'
-      runtime ':hibernate:3.6.10.14'
-
-      build(":release:2.2.1", ":rest-client-builder:1.0.3") {
-            export = false
-      }
-
-      runtime(":fields:1.3") { export = false }
-    }
+    //runtime(":fields:1.4") { export = false }
+  }
 }
