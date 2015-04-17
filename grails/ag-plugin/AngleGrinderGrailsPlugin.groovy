06751f8e45d0eb1280e691b2b719e93e10dff9f8
Ken Roberts <ken@9ci.com>
2015-04-17 08:19:05 -0500

    publish-plugin angle-grinder 0.6.5

--- grails/ag-plugin/AngleGrinderGrailsPlugin.groovy
+++ grails/ag-plugin/AngleGrinderGrailsPlugin.groovy
@@ -18,5 +18,4 @@ A plugin that enables ties grails and angular together
     def groupId = 'nine'
     def issueManagement = [system: 'GitHub', url: 'https://github.com/9ci/angle-grinder/issues']
     def scm = [url: 'https://github.com/9ci/angle-grinder']
-
 }
