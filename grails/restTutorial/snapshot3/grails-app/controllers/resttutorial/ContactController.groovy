6dcb80a535184ef35e468c11e95212dd97c3b66b
Alexey Zvegintcev <alexey.zvegintcev@sysgears.com>
2016-11-23 18:42:59 +0200

    Snapshot 3

---
+++ grails/restTutorial/snapshot3/grails-app/controllers/resttutorial/ContactController.groovy
@@ -0,0 +1,25 @@
+package resttutorial
+
+import grails.plugin.dao.DomainException
+import grails.plugin.dao.RestDaoController
+
+class ContactController extends RestDaoController {
+  static responseFormats = ['json']
+
+  ContactController() {
+    super(Contact)
+  }
+
+  def inactivate() {
+    Contact instance = queryForResource(params.contactId as Long)
+    if (instance == null) {
+      notFound()
+      return
+    }
+
+    Contact contact = dao.inactivate(params.contactId as Long)
+    respond contact
+  }
+
+
+}
--- grails/tutorial/grails-app/assets/javascripts/tutorial.js
+++
@@ -1,8 +0,0 @@
-//= require angleGrinder/vendor.js
-//= require angleGrinder/angleGrinder.js
-//= require application.js
-//= require_tree app/auth
-//= require_tree app/org/controllers/org
-//= require_tree app/org/services
-//= require app/org/orgApp
-//= require app/config
