d58aef0997724834cea17dec9a66b929018dde82
Ken Roberts <kroberts@9ci.com>
2017-02-17 09:25:11 -0600

    publish-plugin angle-grinder 1.0.1

--- grails/ag-plugin/AngleGrinderGrailsPlugin.groovy
+++ grails/ag-plugin/AngleGrinderGrailsPlugin.groovy
@@ -1,6 +1,7 @@
 class AngleGrinderGrailsPlugin {
 
-    def version = '1.0.1-SNAPSHOT'
+    //def version = '1.0.1-SNAPSHOT'
+    def version = '1.0.1'
     def grailsVersion = '2.4.4 > *'
     def dependsOn = [:]
     def pluginExcludes = []
