8cd984fdff4d0cc052f0372392d35822dbb04c77
Joshua B <joshdev@9ci.com>
2020-03-11 04:15:44 -0600

    working scss refactor

--- examples/demo/src/fresh/sidenav/index.js
+++ examples/demo/src/fresh/sidenav/index.js
@@ -1,36 +1,37 @@
 import appState from 'angle-grinder/src/tools/AppState'
 import appRoot from '../../routerStates.js'
-import feather from 'feather-icons'
+import _ from 'lodash'
+//import feather from 'feather-icons'
 //import * as sidenav from './sidenav'
 
 export function toggleSidenav(){
-  appState.sidenav.open = !appState.sidenav.open
-  if(appState.sidenav.open){
-    $(".sidebar-menu .list-item.active ul").slideDown();
-  } else {
-    $(".sidebar-menu .list-item ul").slideUp();
-  }
+  appState.sidenav.open ? closeSidenav() : openSidenav()
+}
+
+function openSidenav(){
+  $(".sidebar-menu .list-item.active .submenu").slideDown()
+  $(".sidebar-menu .list-item.active").addClass("is-open")
+  appState.sidenav.open = true
+}
+
+function closeSidenav(){
+  appState.sidenav.open = false
+  // close submenus
+  $(".sidebar-menu .list-item").removeClass("is-open");
+  $(".sidebar-menu .list-item .submenu").slideUp()
 }
 
 class controller {
 
-  constructor() {
+  constructor($element, $timeout) {
+    this.$timeout = $timeout
     this.$state = appState.$state
     this.appState = appState
     this.sideMenuItems = appRoot.children
   }
 
-  // init code here
-  $onInit() {
-  }
-
   get isOpen() {
     return appState.sidenav.open
-  }
-
-  // checks if item is an open item
-  isItemOpen(mitem) {
-    return this.openItem == mitem.name
   }
 
   // checks if item is the active item
@@ -38,54 +39,53 @@ class controller {
     return this.$state.includes(mitem.name)
   }
 
-  $onChanges(changesObj){
-    console.log("sidebar $onChanges(changesObj)", changesObj)
-  }
-
   toggleSidenav(){
     toggleSidenav()
   }
 
   itemClick(mitem, $event){
+    let $target = $($event.currentTarget)
 
-    //console.log("***********itemClick mitem**********", mitem)
+    //make sure sidenav is open
+    appState.sidenav.open = true
+    console.log("$target", $target)
+
     // if it has children then expand it
     if(mitem.children){
       $event.preventDefault()
+      if (!$target.parent().hasClass("is-open")) {
+        $(".sidebar-menu .list-item .submenu").slideUp();
+        $target.next().slideToggle();
+        $(".sidebar-menu .list-item").removeClass("is-open");
+        $target.parent().addClass("is-open");
+      } else {
+        $target.next().slideToggle();
+        $(".sidebar-menu .list-item").removeClass("is-open");
+      }
       // if openItem is equal item then its open so null out to close it
       this.openItem = (this.openItem === mitem.name) ? '' : mitem.name
-
-      //mitem.active = !mitem.active
     } else {
-      // it has no children then its a link
-      // console.log("appState.$state.go", mitem.name)
+      // it has no children, its a ui-router link
+      closeSidenav()
       appState.$state.go(mitem.name)
-      this.openItem =
     }
   }
 
+  // final run, basically an "after init"
   $postLink() {
-    // feather icons
-    feather.replace()
-
-    //Sidebar menu
-    if ($('.sidebar').length) {
-      // $(".sidebar-menu > .has-children > a").on("click", function (i) {
-      //   i.preventDefault();
-      //   // this in a click function refers to the element that was clicked
-      //   if (!$(this).parent().hasClass("active")) {
-      //     $(".sidebar-menu li ul").slideUp();
-      //     $(this).next().slideToggle();
-      //     $(".sidebar-menu li").removeClass("active");
-      //     $(this).parent().addClass("active");
-      //   }
-      //   else {
-      //     $(this).next().slideToggle();
-      //     $(".sidebar-menu li").removeClass("active");
-      //   }
-      // });
-    }
+    //make sure the submenu is shown for active menu item
+    this.$timeout(function() {
+      $(".sidebar-menu .list-item.active .submenu").show()
+      $(".sidebar-menu .list-item.active").addClass("is-open")
+    })
   }
+  // init code here
+  // $onInit() {
+  //   this.openItem = this.$state.current.name
+  // }
+  // $onChanges(changesObj){
+  //   console.log("sidebar $onChanges(changesObj)", changesObj)
+  // }
 
 }
 
@@ -93,22 +93,5 @@ export default angular.module('demo.fresh.sidenav',[])
   .component('freshSidenav', {
     controller,
     template: require('./index.html'),
-  })
-  .animation('.slide', function() {
-    var NG_HIDE_CLASS = 'ng-hide';
-    return {
-      beforeAddClass: function(element, className, done) {
-        if(className === NG_HIDE_CLASS) {
-          console.log("sliding up")
-          element.slideUp(done);
-        }
-      },
-      removeClass: function(element, className, done) {
-        if(className === NG_HIDE_CLASS) {
-          console.log("sliding down")
-          element.hide().slideDown(done);
-        }
-      }
-    }
   })
   .name
