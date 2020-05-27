2dc299719f19c6d1e3a25806be0c1ad052a859ad
Zveg <zveg90@gmail.com>
2020-05-27 21:33:46 +0300

    refactor select-rest component

--- src/ng/components/ag-select-rest/index.js
+++ src/ng/components/ag-select-rest/index.js
@@ -1,25 +1,96 @@
-import angular from 'angular'
-import AgSelectRestCtrl from './AgSelectRestCtrl'
+/* eslint-disable*/
+import AgBaseControl from '../AgBaseControl'
+import _ from 'lodash'
 import './select-rest.scss'
 
-const MOD_NAME = 'forms.ag-selectRest'
-export default MOD_NAME
-angular
-  .module(MOD_NAME, [])
-  .component('agSelectRest', {
-    transclude: true,
-    template: require('./select-rest.html'),
-    controller: AgSelectRestCtrl,
-    controllerAs: 'cmpCtrl',
-    require: {
-      ngModelCtrl: 'ngModel'
+class Controller extends AgBaseControl {
+  selectOptions = {}
+  opts = {
+    minimumInputLength: 3,
+    closeOnSelect: false,
+    useDataObject: true,
+    width: 'element',
+    ajax: {
+      dataType: 'json',
+      quietMillis: 250,
+      data: function (term, pageNum) { // page is the one-based page number tracked by Select2
+        if (pageNum == null) { pageNum = 1 }
+        return {
+          q: term, //search term
+          page: pageNum, // page number
+          // sorting and pagination
+          sort: 'id',
+          order: 'asc',
+          max: 20
+        }
+      },
+      results: function (result, page) {
+        return { results: result.rows, more: page < result.total }
+      }
     },
-    bindings: {
-      url: '@',
-      selectOptions: '<',
-      multiple: '@',
-      label: '@',
-      name: '@',
-      required: '@'
+    formatResult: this.repoFormatResult,
+    formatSelection: (item) => item.name,
+    initSelection: angular.noop // needs to be here or it blows up setting value
+    //dropdownCssClass: "bigdrop", // apply a special css
+    //escapeMarkup: function (m) { return m; } // don't escape is rendering html
+  }
+
+  /* @ngInject */
+  constructor($element, $timeout, pathWithContext) {
+    super($element)
+    this.$timeout = $timeout
+    this.pathWithContext = pathWithContext
+  }
+
+  $onInit() {
+    super.onInit()
+    _.merge(this.opts, this.selectOptions)
+    this.opts.ajax.url = this.pathWithContext(this.url)
+  }
+
+  repoFormatResult(item) {
+    var markup = `
+      <table class="table table-condensed select-rest-result">
+        <tr>
+          <td>${item.num}</td>
+          <td>${item.name}</td>
+        </tr>
+      </table>
+    `
+    return markup;
+  }
+
+  // if its multiple & closeOnSelect:false then squinky hack so it stays open
+  // https://github.com/select2/select2/issues/2264#issuecomment-213003190
+  $postLink(scope, elm) {
+    if(this.opts.multiple && !this.opts.closeOnSelect){
+      this.$timeout(() => {
+        let inputEl = angular.element(document.getElementById(this.id))
+        inputEl.bind('select2-selecting', function(e) {
+          e.preventDefault()
+          let ngModelCtrl = inputEl.controller('ngModel')
+          var data = ngModelCtrl.$modelValue || []
+          data.push(e.object)
+          ngModelCtrl.$setViewValue(data)
+        })
+      })
     }
-  })
+  }
+
+}
+export default () => ({
+  ...AgBaseControl.common.dir,
+  replace: false,
+  template: require('./select-rest.html'),
+  controller: Controller,
+  require: {
+    ngModelCtrl: 'ngModel',
+    formCtrl: '^agForm'
+  },
+  scope: {
+    ...AgBaseControl.common.scope,
+    url: '@',
+    selectOptions: '<',
+    multiple: '@'
+  }
+})
