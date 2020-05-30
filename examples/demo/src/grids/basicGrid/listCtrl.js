be3239a892b421b14c454b3de1babf6a13da050c
Zveg <zveg90@gmail.com>
2020-05-30 20:54:23 +0300

    selectd rows output

--- examples/demo/src/grids/basicGrid/listCtrl.js
+++ examples/demo/src/grids/basicGrid/listCtrl.js
@@ -1,25 +1,25 @@
 // import BaseCtrl from 'angle-grinder/src/ng/utils/BaseCtrl'
 import { expose } from 'angle-grinder/src/ng/utils/ngHelpers'
 import { generateData } from '../dataGenerator'
-import gridOptions from "../gridOptions";
+import gridOptions from "../gridOptions"
 
 /* @ngInject */
 export default class ListCtrl {
-  constructor($scope, resourceBuilder, DialogCrudCtrlMixin) {
+  constructor($scope, resourceBuilder, DialogCrudCtrlMixin, $log) {
     this.$scope = $scope
-    this.resourceBuilder = resourceBuilder
     this.DialogCrudCtrlMixin = DialogCrudCtrlMixin
+    this.$log = $log
   }
 
   $onInit() {
     // initialize the grid with generated data
     const data = generateData(100)
 
-    const selectedRow = function() { return console.log('exampleGridOptions selected row:', arguments) }.bind(this)
-    this.$scope.gridOptions = gridOptions({ data, onSelectRow: selectedRow, pager: false, datatype: 'local' })
+    const selectedRow = function() { return this.$log.debug('exampleGridOptions selected row:', arguments) }.bind(this)
+    this.gridOptions = gridOptions({ data, onSelectRow: selectedRow, pager: false, datatype: 'local' })
   }
 
   getSelectedRowsData() {
-    return this.$scope.exampleGrid.getSelectedRows()
+    this.selectedRowsData = this.$scope.exampleGrid.getSelectedRows()
   }
 }
