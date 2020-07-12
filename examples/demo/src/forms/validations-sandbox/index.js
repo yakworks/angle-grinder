701ec0560ee18d01f9e07698e2d667bf7f7811d5
Zveg <zveg90@gmail.com>
2020-07-12 12:45:24 +0300

    validation demo example

--- examples/demo/src/forms/validations-sandbox/index.js
+++ examples/demo/src/forms/validations-sandbox/index.js
@@ -1,41 +1,17 @@
-import _ from 'lodash'
-import Swal from 'sweetalert2'
+import compDemoModule from './component'
 
-class ValidationsCtrl {
-  menuDisplay = 'Choose an action'
-
-  menuItems = [
-    { display: '<strong>Action</strong>', action: () => Swal.fire('something special') },
-    { display: 'Another action' },
-    { divider: true },
-    { display: 'Separated link' }
-  ]
-
-  vm = {}
-
-  constructor(serverErrorsService, $http, $timeout) {
-    this.serverErrorsService = serverErrorsService
-    this.$http = $http
-    this.$timeout = $timeout
-  }
-
-  menuItemClick = function(menuItem, e) {
-    console.log('menuItemClick', { menuItem, e })
-  }
-
-  save(form) {
-    this.$http.post('http://localhost:3000/validation/mock', this.vm).then(resp => {
-      console.log('all saved', resp)
-    }, errorResp => {
-      this.serverErrorsService.setErrors(form, errorResp)
-      console.log('form has validation errors', errorResp)
-    })
-  }
-}
-
-export default angular.module('demo.validationsExample', [])
-  .component('validationsExample', {
-    controller: ValidationsCtrl,
-    template: require('./validations.comp.html')
-  })
-  .name
+const template = `
+<example-snippet raw-js='$ctrl.rawJs' raw-html='$ctrl.rawHtml' raw-md='$ctrl.rawMd'>
+  <validation-demo></validation-demo>
+</example-snippet>
+`
+// export module name
+export default angular
+  .module(compDemoModule)
+  .component('validationDemoIndex', {
+    template: template,
+    controller: function() {
+      this.rawHtml = require('./component.html')
+      this.rawJs = require('!raw-loader!./component.js').default
+    }
+  }).name
