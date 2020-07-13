6dc5e2978bce11db0c6021d74b215c8abda6bfa0
Joshua B <joshdev@9ci.com>
2020-07-13 10:40:37 -0600

    added default dataStoreApi to demo

--- examples/demo/src/store/index.js
+++ examples/demo/src/store/index.js
@@ -3,6 +3,7 @@ import {RestStoreApi} from "./RestStoreApi";
 
 // export module name
 export default angular.module('ag.demo.api', [])
+  .service('dataStoreApi', LocalStoreApi) //this is the default, used in components
   .service('localStoreApi', LocalStoreApi)
   .service('restStoreApi', RestStoreApi)
   .name
