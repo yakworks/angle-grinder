c8d07a1c72cda0f36f81085b7725fd7b961d827c
Zveg <zveg90@gmail.com>
2020-07-21 19:53:45 +0300

    Rest controller for gorm-rest

---
+++ examples/ag-demo-grails/grails-app/controllers/agdemo/api/BaseDomainControllerTrait.groovy
@@ -0,0 +1,68 @@
+package agdemo.api
+
+import gorm.tools.Pager
+import grails.converters.JSON
+import grails.core.GrailsApplication
+import org.grails.plugins.appsetupconfig.AppSetupService
+
+trait BaseDomainControllerTrait {
+    AppSetupService appSetupService
+    GrailsApplication grailsApplication
+    def getDomainClass() {
+        this.entityClass
+    }
+    List<String> getPickShowFields() { return ['*'] }
+    List<String> getPicSearchableFields() { return [] }
+    Map defaultGridOptions = [colModel: [[name: 'id']]]
+
+    def index() {
+        listJson()
+    }
+
+    def listJson() {
+        println()
+        def pageData = pagedList(listCriteria())
+        render pageData.jsonData as JSON
+    }
+
+    def pagedList(dlist) {
+        def pageData = new Pager(params)
+        def fieldList
+        if (hasProperty('selectFields')) {
+            fieldList = selectFields
+        }
+        pageData.setupData(dlist, fieldList)
+        return pageData
+    }
+
+    Map getExternalConfig() {
+        ConfigObject controllerConfig = grailsApplication.setupConfig.screens."$controllerName"
+        return controllerConfig
+    }
+
+    Map getGridOptions() {
+        def options = externalConfig.list.gridz
+        if (!options) {
+            return defaultGridOptions
+        }
+        return appSetupService.getValue(options)
+    }
+
+    def gridOptions() {
+        String gridOptsJson = gridOptions as JSON
+        render(gridOptsJson)
+    }
+
+    /**
+     * returns the list of domain obects for the scaffolded contro
+     */
+    def listCriteria() {
+        def crit = this.entityClass.createCriteria()
+        def pager = new Pager(params)
+        def datalist = crit.list(max: pager.max, offset: pager.offset) {
+            if (params.sort)
+                order(params.sort, params.order)
+        }
+        return datalist
+    }
+}
---
+++ examples/ag-demo-grails/grails-app/controllers/agdemo/api/OrgApiController.groovy
@@ -0,0 +1,68 @@
+package agdemo.api
+
+import agdemo.Org
+import gorm.restapi.controller.RestApiRepoController
+import gorm.tools.Pager
+import gorm.tools.hibernate.criteria.CriteriaUtils
+import grails.converters.JSON
+
+class OrgApiController extends RestApiRepoController<Org> implements BaseDomainControllerTrait {
+    OrgApiController() {
+        super(Org, false)
+    }
+    def selectFields = ["*"]
+
+    def listCriteria() {
+        def pager = new Pager(params)
+        def crit = Org.createCriteria()
+
+        def filters = params.filters ? JSON.parse(params.filters) : null
+        def qslike = (filters?.quickSearch) ? (filters?.quickSearch + "%") : null
+
+        def datalist = crit.list(max: pager.max, offset: pager.offset) {
+            if (qslike) {
+                or {
+                    ilike 'name', qslike
+                    ilike 'num', qslike
+                }
+            }
+
+            if (filters?.name)
+                ilike 'name', filters.name + "%"
+
+            if (filters?.num)
+                ilike 'num', filters.num + "%"
+            if (filters?.id)
+                eq 'id', filters.id.toLong()
+            if (filters?.ids)
+                inList 'id', (filters.ids as Long[])
+            if (filters?.zone)
+                eq 'timeZone', filters.zone
+            if (filters?.zones)
+                inList 'timeZone', (filters.zones as String[])
+           // CriteriaUtils.applyOrder(params, delegate)
+        }
+
+        return datalist
+    }
+
+    def pickList() {
+        def pager = new Pager(params)
+        def crit = Org.createCriteria()
+        def qslike = (params.q) ? (params.q + "%") : null
+        def datalist = crit.list(max: pager.max, offset: pager.offset) {
+            if (qslike && qslike != '*%') {
+                or {
+                    ilike 'name', qslike
+                    ilike 'num', qslike
+                }
+            }
+
+            if (params.sort)
+                order(params.sort, params.order)
+        }
+
+        def pagedList = pagedList(datalist)
+        render pagedList.jsonData as JSON
+    }
+}
