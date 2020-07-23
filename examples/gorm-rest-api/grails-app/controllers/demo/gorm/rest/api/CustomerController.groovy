a4117b95c352775bec8f3af0e2bfdc0b4a3405b5
Zveg <zveg90@gmail.com>
2020-07-23 20:13:31 +0300

    grid search

--- examples/gorm-rest-api/grails-app/controllers/demo/gorm/rest/api/CustomerController.groovy
+++ examples/gorm-rest-api/grails-app/controllers/demo/gorm/rest/api/CustomerController.groovy
@@ -3,6 +3,7 @@ package demo.gorm.rest.api
 import gorm.restapi.controller.RestApiRepoController
 import gorm.tools.Pager
 import grails.converters.JSON
+import org.grails.datastore.mapping.query.api.BuildableCriteria
 
 class CustomerController extends RestApiRepoController<Customer> {
     static allowedMethods = [list  : ["GET", "POST"], create: "POST",
@@ -16,7 +17,7 @@ class CustomerController extends RestApiRepoController<Customer> {
     def index() {
         Pager pager = new Pager(params)
 
-        Pager pagedList = pager.setupData(Customer.list(), ['*'])
+        Pager pagedList = pager.setupData(listCriteria(), ['*'])
         Map resp =pagedList.jsonData
         resp.data = resp.rows
         respond resp
@@ -25,6 +26,38 @@ class CustomerController extends RestApiRepoController<Customer> {
     // TODO fix urlMapping to make it work
     def customAction() {
         render 'Yeah it works'
+    }
+
+    List listCriteria() {
+        Pager pager = new Pager(params)
+        BuildableCriteria crit = entityClass.createCriteria()
+
+        Map filters = params.filters ? JSON.parse(params.filters) as Map : null
+        def qslike = (filters?.quickSearch) ? (filters?.quickSearch + "%") : null
+
+        List datalist = crit.list(max: pager.max, offset: pager.offset) {
+            if (qslike) {
+                or {
+                    ilike 'name', qslike
+                    ilike 'num', qslike
+                }
+            }
+            if (filters?.city)
+                ilike 'city', filters.city + "%"
+            if (filters?.country)
+                ilike 'country', filters.country + "%"
+            if (filters?.state)
+                ilike 'state', filters.state + "%"
+
+            if (filters?.name)
+                ilike 'name', filters.name.split(',')
+            if (filters?.num)
+                'inList' 'num', filters.num.split(',')
+            if (filters?.timezone)
+                eq 'timezone', filters.zontimezonee
+        }
+
+        return datalist
     }
 
 }
