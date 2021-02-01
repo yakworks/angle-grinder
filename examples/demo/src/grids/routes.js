eafd9e9e0ef08c87624147ecdac4316a7e8437a6
alexeyzvegintcev <alexey.zvegintcev@sysgears.com>
2021-02-01 19:05:12 +0200

    571 split router states (#572)
    * Fixed import path
    
    * Split routerStates to separate files #571
    
    * Fixed typo
    
    Co-authored-by: Zveg <zveg90@gmail.com>

---
+++ examples/demo/src/grids/routes.js
@@ -0,0 +1,67 @@
+const gridsStates = {
+  name: 'grids',
+  abstract: true,
+  template: '<div ui-view class="fade-in-up"></div>',
+  data: {
+    icon: 'mdi mdi-table-large'
+  },
+  children: [
+    {
+      name: 'vanilla-agGridList',
+      data: { title: 'Vanilla rest agGridList'},
+      abstract: 'fresh.grid.vanilla-agGridList.list',
+      url: '/customer',
+      children: [{
+        name: 'list',
+        isMenuItem: false,
+        url: '',
+        component: 'agGridList'
+      },{
+        name: 'Edit Customer',
+        isMenuItem: false,
+        url: '/:id',
+        component: 'customRestEditDemo'
+      }],
+      resolve: {
+        apiKey: () => "customer",
+        notification: () => ({
+          class: "is-primary is-light",
+          text: "Uses ui-router to send rest apiKey to generic agGridList component"
+        })
+      }
+    },
+    {
+      name: 'editOnly-agGridList',
+      url: '/tag',
+      component: 'agGridList',
+      data: { title: 'Edit Only agGridList'},
+      resolve: {
+        apiKey: () => "tag",
+        notification: () => ({
+          class: "is-primary is-light",
+          text: "Uses ui-router to send rest apiKey to generic agGridList component, configured to only allow editing desc"
+        })
+      }
+    },
+    {
+      name: 'override-rest-grid',
+      data: { title: 'Custom Grid Component'},
+      component: 'basicGridRestIndex'
+    },
+    {
+      name: 'basic-grid',
+      component: 'basicGridIndex',
+      url: '/foo'
+    },
+    {
+      name: 'Legacy Demo Grid',
+      component: 'legacyGridIndex'
+    }
+    // {
+    //   name: 'Rest Demo Grid',
+    //   component: 'restGridExample'
+    // }
+  ]
+}
+
+export default gridsStates
