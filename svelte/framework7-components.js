5bee728836cfef45ffdeb35dbe91efea986ca4e5
Joshua B <joshdev@9ci.com>
2021-09-27 19:04:22 -0600

    fix grid pager icons, move back to framework7-svelte instead of source

--- svelte/framework7-components.js
+++ svelte/framework7-components.js
@@ -1,25 +1,25 @@
 //we only grab what we want to make it lighter
 
-export { default as App } from 'framework7-source/src/svelte/components/app.svelte';
+export { default as App } from 'framework7-svelte/esm/svelte/app.svelte';
 
-export { default as Block } from 'framework7-source/src/svelte/components/block.svelte';
-export { default as BlockHeader } from 'framework7-source/src/svelte/components/block-header.svelte';
-export { default as BlockFooter } from 'framework7-source/src/svelte/components/block-footer.svelte';
-export { default as BlockTitle } from 'framework7-source/src/svelte/components/block-title.svelte';
+export { default as Block } from 'framework7-svelte/esm/svelte/block.svelte';
+export { default as BlockHeader } from 'framework7-svelte/esm/svelte/block-header.svelte';
+export { default as BlockFooter } from 'framework7-svelte/esm/svelte/block-footer.svelte';
+export { default as BlockTitle } from 'framework7-svelte/esm/svelte/block-title.svelte';
 
-export { default as Icon } from 'framework7-source/src/svelte/components/icon.svelte';
-export { default as Link } from 'framework7-source/src/svelte/components/link.svelte';
+export { default as Icon } from 'framework7-svelte/esm/svelte/icon.svelte';
+export { default as Link } from 'framework7-svelte/esm/svelte/link.svelte';
 
-export { default as CardContent } from 'framework7-source/src/svelte/components/card-content.svelte';
-export { default as CardFooter } from 'framework7-source/src/svelte/components/card-footer.svelte';
-export { default as CardHeader } from 'framework7-source/src/svelte/components/card-header.svelte';
-export { default as Card } from 'framework7-source/src/svelte/components/card.svelte';
+export { default as CardContent } from 'framework7-svelte/esm/svelte/card-content.svelte';
+export { default as CardFooter } from 'framework7-svelte/esm/svelte/card-footer.svelte';
+export { default as CardHeader } from 'framework7-svelte/esm/svelte/card-header.svelte';
+export { default as Card } from 'framework7-svelte/esm/svelte/card.svelte';
 
-export { default as ListButton } from 'framework7-source/src/svelte/components/list-button.svelte';
-export { default as ListGroup } from 'framework7-source/src/svelte/components/list-group.svelte';
-export { default as ListIndex } from 'framework7-source/src/svelte/components/list-index.svelte';
-export { default as ListInput } from 'framework7-source/src/svelte/components/list-input.svelte';
-export { default as ListItemCell } from 'framework7-source/src/svelte/components/list-item-cell.svelte';
-export { default as ListItemRow } from 'framework7-source/src/svelte/components/list-item-row.svelte';
-export { default as ListItem } from 'framework7-source/src/svelte/components/list-item.svelte';
-export { default as List } from 'framework7-source/src/svelte/components/list.svelte';
+export { default as ListButton } from 'framework7-svelte/esm/svelte/list-button.svelte';
+export { default as ListGroup } from 'framework7-svelte/esm/svelte/list-group.svelte';
+export { default as ListIndex } from 'framework7-svelte/esm/svelte/list-index.svelte';
+export { default as ListInput } from 'framework7-svelte/esm/svelte/list-input.svelte';
+export { default as ListItemCell } from 'framework7-svelte/esm/svelte/list-item-cell.svelte';
+export { default as ListItemRow } from 'framework7-svelte/esm/svelte/list-item-row.svelte';
+export { default as ListItem } from 'framework7-svelte/esm/svelte/list-item.svelte';
+export { default as List } from 'framework7-svelte/esm/svelte/list.svelte';
--- svelte/framework7.js
+++ svelte/framework7.js
@@ -1,6 +1,6 @@
-import Framework7Svelte from 'framework7-source/src/svelte/shared/plugin';
-import { f7, f7ready, theme, app } from 'framework7-source/src/svelte/shared/f7';
-import { useStore } from 'framework7-source/src/svelte/shared/use-store';
+import Framework7Svelte from 'framework7-svelte/esm/shared/plugin';
+import { f7, f7ready, theme, app } from 'framework7-svelte/esm/shared/f7';
+import { useStore } from 'framework7-svelte/esm/shared/use-store';
 
 export { f7, f7ready, theme, useStore, app };
 export default Framework7Svelte;
